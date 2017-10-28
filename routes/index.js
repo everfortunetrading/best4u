d5a7666114e26c12df264b6dd094c155e597ef23
ysera11 <hanyuzhu94@gmail.com>
2017-10-28 16:48:12 -0700

    merged the sample front end with routing

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/no-sidebar', function(req, res, next) {
+  res.render('no-sidebar', { title: 'Express' });
+});
+
+
+
 module.exports = router;
