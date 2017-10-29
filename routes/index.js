4fdb05df4e76b0d0c048eb28bae522dd684654bb
Andrew <acleung@cpp.edu>
2017-10-28 18:00:25 -0700

    frontend work

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', content);
 });
 
 router.get('/no-sidebar', function(req, res, next) {
   res.render('no-sidebar', { title: 'Express' });
 });
+
+content = {
+  title: 'Best 4 U',
+  subtitle: 'Travel Mugs and Bottles'
+}
 
 
 
