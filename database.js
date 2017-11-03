356da499e4b56521ef5ef0325376143f7eb9a395
ysera11 <hanyuzhu94@gmail.com>
2017-11-03 14:44:42 -0700

    added databse connection

--- database.js
+++ database.js
@@ -1,12 +1,25 @@
 var mysql = require('mysql')
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword"
-});
+function createDBConnection(){
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Chrishanyu94",
+    database: "mydb"
+  })
+
+  return con;
+}
+
+var con = createDBConnection();
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  con.query("CREATE TABLE test (name VARCHAR(255), address VARCHAR(255))", function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
 });
+
+module.exports = db;
