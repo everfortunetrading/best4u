2c4f29dc348fb8b5e613c057e331dbb17ff75bf7
Andrew <acleung@cpp.edu>
2017-11-03 14:47:21 -0700

    admin

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,17 @@ router.get('/no-sidebar', function(req, res, next) {
   res.render('no-sidebar', { title: 'Express' });
 });
 
+router.get('/admin', function(req, res, next) {
+  res.render('admin');
+})
+
+router.post('/finished', (req, res) => {
+  /*product = req.body;
+  product.Name != "" & product.Description != "" ? res.send(product.Name + " added!") : res.send("Nothing was added! Please fill in entirely.");
+  var tmp_path = req.files.image.path
+  res.send(tmp_path) */
+})
+
 content = {
   title: 'Best 4 U',
   subtitle: 'Travel Mugs and Bottles'
