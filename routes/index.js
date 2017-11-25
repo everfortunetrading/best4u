fc732c1ef104542bd2e113c33222526fb8cef9ab
ysera11 <hanyuzhu94@gmail.com>
2017-11-24 16:44:58 -0800

    added index ejs ability to read from database

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../database');
+
+var con = db()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', content);
+  con.query("SELECT * FROM products", function (err, result, fields) {
+    if (err) throw err;
+    content = {
+      title: 'Best 4 U',
+      subtitle: 'Travel Mugs and Bottles',
+      products: result
+    }
+    res.render('index', content);
+  });
+
 });
 
 router.get('/no-sidebar', function(req, res, next) {
   res.render('no-sidebar', { title: 'Express' });
 });
-
-content = {
-  title: 'Best 4 U',
-  subtitle: 'Travel Mugs and Bottles'
-}
 
 module.exports = router;
